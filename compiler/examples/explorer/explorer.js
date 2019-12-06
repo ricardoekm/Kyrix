db48d9438e9484e41f5f69419231621cbf63a158
Ricardo Mayerhofer <ricardo.ekm@gmail.com>
2019-12-06 16:42:16 -0300

    Moving rendering and transform to a separate file.

--- compiler/examples/explorer/explorer.js
+++ compiler/examples/explorer/explorer.js
@@ -4,49 +4,14 @@ const Canvas = require("../../src/Canvas").Canvas;
 const Jump = require("../../src/Jump").Jump;
 const Layer = require("../../src/Layer").Layer;
 const View = require("../../src/View").View;
-const Transform = require("../../src/Transform").Transform;
 
-const transform = new Transform(
-    "select * from income;",
-    "explorer",
-    function(row) {
-        var ret = [];
-        ret.push(row[0]);
-        ret.push(row[1]);
-
-        return Java.to(ret, "java.lang.String[]");
-    },
-    ["age", "income"],
-    true
-);
-
-const rendering = function(svg, data) {
-    g = svg.append("g");
-    g.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d) {
-            return Math.floor(Math.random() * 1000);
-        })
-        .attr("cy", function(d) {
-            return Math.floor(Math.random() * 1000);
-        })
-        .attr("r", 10)
-        .attr("fill", "#145bce");
-};
-
-var layer = new Layer(transform, true);
-layer.addRenderingFunc(rendering);
-
-var canvas = new Canvas("scatterplot", 1000, 1000);
-canvas.addLayer(layer);
+const scatterplot = require("./scatterplot");
 
 var project = new Project("explorer", "../config.txt");
-project.addCanvas(canvas);
+project.addCanvas(scatterplot.canvas);
 
 var view = new View("explorer", 0, 0, 1000, 1000);
 project.addView(view);
-project.setInitialStates(view, canvas, 0, 0);
+project.setInitialStates(view, scatterplot.canvas, 0, 0);
 
 project.saveProject();
--- compiler/examples/explorer/scatterplot.js
+++ compiler/examples/explorer/scatterplot.js
@@ -4,19 +4,45 @@ const Canvas = require("../../src/Canvas").Canvas;
 const Jump = require("../../src/Jump").Jump;
 const Layer = require("../../src/Layer").Layer;
 const View = require("../../src/View").View;
+const Transform = require("../../src/Transform").Transform;
 
-const rendering = (svg,data) => {
-    if (number < 0) {
-      return -number;
-    }
-    return number;
-  }
+const transform = new Transform(
+    "select * from income;",
+    "explorer",
+    function(row) {
+        var ret = [];
+        ret.push(row[0]);
+        ret.push(row[1]);
+
+        return Java.to(ret, "java.lang.String[]");
+    },
+    ["age", "income"],
+    true
+);
+
+const rendering = function(svg, data) {
+    g = svg.append("g");
+    g.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {
+            return Math.floor(Math.random() * 1000);
+        })
+        .attr("cy", function(d) {
+            return Math.floor(Math.random() * 1000);
+        })
+        .attr("r", 10)
+        .attr("fill", "#145bce");
+};
+
+var layer = new Layer(transform, true);
+layer.addRenderingFunc(rendering);
 
 var canvas = new Canvas("scatterplot", 1000, 1000);
-
-var layer = new Layer(transforms.teamLogoTransform, true);
 canvas.addLayer(layer);
-teamLogoLayer.addRenderingFunc(rendering);
 
-
+module.exports = {
+    canvas
+};
 
